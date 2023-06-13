7cec3089883d58a4204cd77cc4bbe3edf8f5591d
Wilder <lilianbataille6263@gmail.com>
2023-06-13 12:27:52 +0200

    express8

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // charge les variables d'envrionnement à partir de .env
 require("dotenv").config();
 
+const jwt = require("jsonwebtoken");
 const express = require("express");
 const app = express();
 // utilse le middleware pour analyser les données au format JSON
 app.use(express.json());
 
 // j'importe les modules et gestionnaires de routes
-const { hashPassword } = require("./auth");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth");
 const userHandlers = require("./userHandlers");
 const movieHandlers = require("./movieHandlers");
 
@@ -21,17 +22,48 @@ const welcome = (req, res) => {
 
 app.get("/", welcome);
 
+const isItDwight = (req, res)=>{
+  // je vérifie que le mail et le mdp correspondent
+  if(req.body.email === "dwight@theoffice.com" && req.body.password === "123456"){
+    // je crée un objet pour stocker l'adresse mail 
+   const payload = {sub: req.body.email};
+
+   // Génère un token JWT en utilisant l'objet, une clé secrète et une expiration d'une heure
+   const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+   });
+   // envoie une réponse contenant le token
+    res.send({token});
+  }else{
+    // si l'adresse mail ou le mdp ne correspondent pas renvoyer une erreur
+    res.sendStatus(401);
+  }
+};
+
+app.post("/api/login", isItDwight);
+
+
 
 //gère les routes pour les films
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
+
+app.post("/api/login",userHandlers.getUserByEmailWithPasswordAndPassToNext,verifyPassword);
+
+app.use(verifyToken);
+
+app.post("/api/movies", verifyToken, movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 //gère les routes pour les users
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+
+app.post("/api/login",userHandlers.getUserByEmailWithPasswordAndPassToNext,verifyPassword);
+
+app.use(verifyToken);
+
 app.post("/api/users",hashPassword, userHandlers.postUser);
 app.put("/api/users/:id",hashPassword, userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
--- auth.js
+++ auth.js
@@ -1,10 +1,11 @@
-// j'importe le module argon2
+// j'importe les modules argon2 et jsonwebtoken
 const argon2 = require("argon2");
+const jwt = require("jsonwebtoken");
 // fonction asynchrone pour hacher le mot de passe
 const hashPassword = async (req, res, next) => {
   try {
     // extrait le mdp de la requête
-    const {password}= req.body;
+    const { password } = req.body;
 
     //options de hachage du mot de passe
     const hashingOptions = {
@@ -20,15 +21,63 @@ const hashPassword = async (req, res, next) => {
     req.body.hashedPassword = hashedPassword;
     // supprime le mot de passe du corps de la requête
     delete req.body.password;
-    // passe à la fonction middleware suivante
+
     next();
-  } catch (error) {
-    //gère les erreurs
-    console.error(error);
+  } catch (err) {
+    console.error(err);
     res.sendStatus(500);
   }
 };
-// j'exporte la fonction
+const verifyPassword = async (req, res) => {
+  try {
+    const isVerified = await argon2.verify(
+      req.user.hashedPassword,
+      req.body.password
+    );
+
+    if (isVerified) {
+      const payload = { sub: req.user.id };
+
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+
+      delete req.user.hashedPassword;
+      res.send({ token, user: req.user });
+    } else {
+      res.sendStatus(401);
+    }
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+};
+
+const verifyToken = (req, res, next) => {
+  try {
+    const authorizationHeader = req.get("Authorization");
+
+    if (authorizationHeader == null) {
+      throw new Error("Authorization header is missing");
+    }
+
+    const [type, token] = authorizationHeader.split(" ");
+
+    if (type !== "Bearer") {
+      throw new Error("Authorization header has not the 'Bearer' type");
+    }
+
+    req.payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    next();
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(401);
+  }
+};
+
 module.exports = {
   hashPassword,
+  verifyPassword,
+  verifyToken, 
 };
